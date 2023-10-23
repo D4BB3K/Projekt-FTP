f84516a4da3fbb73ed2d9d4ab1b181d8a8495d8d
D4BB3K <d4bb3k.biznes@gmail.com>
2023-10-23 16:38:34 +0200

    main

--- script/main.js
+++ script/main.js
@@ -18,175 +18,182 @@ function myFunction() {
     }
 }
 
-function fixTable() {
-    const table = document.querySelector('.tabela');
+{
+    function fixTable() {
+        const table = document.querySelector('table');
 
-    // Remove the first row from the table
-    const firstRow = table.querySelector('.indexhead');
-    firstRow.parentNode.removeChild(firstRow);
+        // Remove <hr>s
+        Array.from(table.querySelectorAll('hr')).forEach(({ parentNode }) => {
+            const row = parentNode.parentNode;
+            row.parentNode.removeChild(row);
+        });
 
-    // Make a table head.
-    const thead = document.createElement('thead');
-    thead.appendChild(firstRow);
-    table.insertBefore(thead, table.firstElementChild);
+        // Make a table head.
+        const thead = document.createElement('thead');
+        const firstRow = table.querySelector('.indexhead');
+        firstRow.parentNode.removeChild(firstRow);
+        thead.appendChild(firstRow);
+        table.insertBefore(thead, table.firstElementChild);
 
-    // Remove the first column and put the image in the next.
-    const rows = Array.from(table.querySelectorAll('tr'));
-    rows.forEach((row) => {
-        const iconColumn = row.children[0];
-        const fileColumn = row.children[1];
+        // Remove the first column and put the image in the next.
+        const rows = Array.from(table.querySelectorAll('tr'));
+        rows.forEach((row) => {
+            const iconColumn = row.children[0];
+            const fileColumn = row.children[1];
 
-        // Remove icon column.
-        row.removeChild(iconColumn);
+            // Remove icon column.
+            row.removeChild(iconColumn);
 
-        const image = iconColumn.querySelector('img');
+            const image = iconColumn.querySelector('img');
 
-        if (image) {
-            // Wrap icon in a div.img-wrap.
-            const div = document.createElement('div');
-            div.className = 'img-wrap';
-            div.appendChild(image);
+            if (image) {
+                // Wrap icon in a div.img-wrap.
+                const div = document.createElement('div');
+                div.className = 'img-wrap';
+                div.appendChild(image);
 
-            // Insert icon before filename.
-            fileColumn.insertBefore(div, fileColumn.firstElementChild);
-        }
-    });
-}
-
-// Underscore string's titleize.
-function titleize(str) {
-    return decodeURI(str).toLowerCase().replace(/(?:^|\s|-)\S/g, c => c.toUpperCase());
-}
-
-function addTitle() {
-    let path = window.location.pathname.replace(/\/$/g, '');
-    let titleText;
-
-    if (path) {
-        const parts = path.split('/');
-        path = parts[parts.length - 1];
-        titleText = titleize(path).replace(/-|_/g, ' ');
-    } else {
-        titleText = window.location.host;
+                // Insert icon before filename.
+                fileColumn.insertBefore(div, fileColumn.firstElementChild);
+            }
+        });
     }
 
-    titleText = `Index of ${titleText}`;
+    // Underscore string's titleize.
+    function titleize(str) {
+        return decodeURI(str).toLowerCase().replace(/(?:^|\s|-)\S/g, c => c.toUpperCase());
+    }
 
-    const container = document.createElement('div');
-    container.className = 'header';
-    const h1 = document.createElement('h1');
-    h1.appendChild(document.createTextNode(titleText));
-    container.appendChild(h1);
+    function addTitle() {
+        let path = window.location.pathname.replace(/\/$/g, '');
+        let titleText;
 
-    document.body.insertBefore(container, document.body.firstChild);
-    document.title = titleText;
-}
-
-function getTimeSince(seconds) {
-    let intervalType;
-
-    let interval = Math.floor(seconds / 31536000);
-    if (interval >= 1) {
-        intervalType = 'year';
-    } else {
-        interval = Math.floor(seconds / 2592000);
-        if (interval >= 1) {
-            intervalType = 'month';
+        if (path) {
+            const parts = path.split('/');
+            path = parts[parts.length - 1];
+            titleText = titleize(path).replace(/-|_/g, ' ');
         } else {
-            interval = Math.floor(seconds / 86400);
+            titleText = window.location.host;
+        }
+
+        titleText = `Index of ${titleText}`;
+
+        const container = document.createElement('div');
+        container.className = 'header';
+        const h1 = document.createElement('h1');
+        h1.appendChild(document.createTextNode(titleText));
+        container.appendChild(h1);
+
+        document.body.insertBefore(container, document.body.firstChild);
+        document.title = titleText;
+    }
+
+    function getTimeSince(seconds) {
+        let intervalType;
+
+        let interval = Math.floor(seconds / 31536000);
+        if (interval >= 1) {
+            intervalType = 'year';
+        } else {
+            interval = Math.floor(seconds / 2592000);
             if (interval >= 1) {
-                intervalType = 'day';
+                intervalType = 'month';
             } else {
-                interval = Math.floor(seconds / 3600);
+                interval = Math.floor(seconds / 86400);
                 if (interval >= 1) {
-                    intervalType = 'hour';
+                    intervalType = 'day';
                 } else {
-                    interval = Math.floor(seconds / 60);
+                    interval = Math.floor(seconds / 3600);
                     if (interval >= 1) {
-                        intervalType = 'minute';
+                        intervalType = 'hour';
                     } else {
-                        interval = seconds;
-                        intervalType = 'second';
+                        interval = Math.floor(seconds / 60);
+                        if (interval >= 1) {
+                            intervalType = 'minute';
+                        } else {
+                            interval = seconds;
+                            intervalType = 'second';
+                        }
                     }
                 }
             }
         }
+
+        if (interval > 1 || interval === 0) {
+            intervalType += 's';
+        }
+
+        return `${interval} ${intervalType}`;
     }
 
-    if (interval > 1 || interval === 0) {
-        intervalType += 's';
-    }
+    function fixTime() {
+        const dates = Array.from(document.querySelectorAll('.indexcollastmod'));
+        const now = new Date();
+        dates.forEach((date, i) => {
+            const stamp = date.textContent.trim();
+            if (!stamp || i === 0) return;
 
-    return `${interval} ${intervalType}`;
-}
+            // 2014-12-09 10:43 -> 2014, 11, 09, 10, 43, 0.
+            const parts = stamp.split(' ');
+            const day = parts[0].split('-');
+            const timeOfDay = parts[1].split(':');
+            const year = parseInt(day[0], 10);
+            const month = parseInt(day[1], 10) - 1;
+            const _day = parseInt(day[2], 10);
+            const hour = parseInt(timeOfDay[0], 10);
+            const minutes = parseInt(timeOfDay[1], 10);
 
-function fixTime() {
-    const dates = Array.from(document.querySelectorAll('.indexcollastmod'));
-    const now = new Date();
-    dates.forEach((date, i) => {
-        const stamp = date.textContent.trim();
-        if (!stamp || i === 0) return;
-
-        // 2014-12-09 10:43 -> 2014, 11, 09, 10, 43, 0.
-        const parts = stamp.split(' ');
-        const day = parts[0].split('-');
-        const timeOfDay = parts[1].split(':');
-        const year = parseInt(day[0], 10);
-        const month = parseInt(day[1], 10) - 1;
-        const _day = parseInt(day[2], 10);
-        const hour = parseInt(timeOfDay[0], 10);
-        const minutes = parseInt(timeOfDay[1], 10);
-
-        const time = new Date(year, month, _day, hour, minutes, 0);
-        const difference = Math.round((now.getTime() - time.getTime()) / 1000);
-        date.textContent = `${getTimeSince(difference)} ago`;
-    });
-}
-
-function addSearch() {
-    const input = document.createElement('input');
-    input.type = 'search';
-    input.id = 'search';
-    input.setAttribute('placeholder', 'Search');
-    document.querySelector('.header').appendChild(input);
-
-    const sortColumns = Array.from(document.querySelectorAll('thead a'));
-    const nameColumns = Array.from(document.querySelectorAll('tbody .indexcolname'));
-    const rows = nameColumns.map(({ parentNode }) => parentNode);
-    const fileNames = nameColumns.map(({ textContent }) => textContent);
-
-    function filter(value) {
-        // Allow tabbing out of the search input and skipping the sort links
-        // when there is a search value.
-        sortColumns.forEach((link) => {
-            if (value) {
-                link.tabIndex = -1;
-            } else {
-                link.removeAttribute('tabIndex');
-            }
-        });
-
-        // Test the input against the file/folder name.
-        let even = false;
-        fileNames.forEach((name, i) => {
-            if (!value || name.toLowerCase().includes(value.toLowerCase())) {
-                const className = even ? 'even' : '';
-                rows[i].className = className;
-                even = !even;
-            } else {
-                rows[i].className = 'hidden';
-            }
+            const time = new Date(year, month, _day, hour, minutes, 0);
+            const difference = Math.round((now.getTime() - time.getTime()) / 1000);
+            date.textContent = `${getTimeSince(difference)} ago`;
         });
     }
 
-    document.getElementById('search').addEventListener('input', ({ target }) => {
-        filter(target.value);
-    });
+    function addSearch() {
+        const input = document.createElement('input');
+        input.type = 'search';
+        input.id = 'search';
+        input.setAttribute('placeholder', 'Search');
+        document.querySelector('.header').appendChild(input);
 
-    filter('');
+        const sortColumns = Array.from(document.querySelectorAll('.indexhead a'));
+        const nameColumns = Array.from(document.querySelectorAll('.tabela .indexcolname'));
+        const rows = nameColumns.map(({ parentNode }) => parentNode);
+        const fileNames = nameColumns.map(({ textContent }) => textContent);
+
+        function filter(value) {
+            // Allow tabbing out of the search input and skipping the sort links
+            // when there is a search value.
+            sortColumns.forEach((link) => {
+                if (value) {
+                    link.tabIndex = -1;
+                } else {
+                    link.removeAttribute('tabIndex');
+                }
+            });
+
+            // Test the input against the file/folder name.
+            let even = false;
+            fileNames.forEach((name, i) => {
+                if (!value || name.toLowerCase().includes(value.toLowerCase())) {
+                    const className = even ? 'even' : '';
+                    rows[i].className = className;
+                    even = !even;
+                } else {
+                    rows[i].className = 'hidden';
+                }
+            });
+        }
+
+        document.getElementById('search').addEventListener('input', ({ target }) => {
+            filter(target.value);
+        });
+
+        filter('');
+    }
+
+    fixTable();
+    addTitle();
+    //fixTime();
+    addSearch();
 }
 
-fixTable();
-addTitle();
-// fixTime();
-addSearch();
