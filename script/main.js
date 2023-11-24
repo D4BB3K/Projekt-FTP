09e076851cf0c8e54522fa3041d638587f60cc68
D4BB3K <d4bb3k.biznes@gmail.com>
2023-11-24 14:14:19 +0100

    main

--- script/main.js
+++ script/main.js
@@ -24,45 +24,53 @@ function myFunction() {
 {
   function fixTable() {
     const table = document.querySelector('table');
-
+  
     // Remove <hr>s
     Array.from(table.querySelectorAll('hr')).forEach(({ parentNode }) => {
       const row = parentNode.parentNode;
       row.parentNode.removeChild(row);
     });
-
+  
     // Make a table head.
     const thead = document.createElement('thead');
     const firstRow = table.querySelector('tr');
-    firstRow.parentNode.removeChild(firstRow);
-    thead.appendChild(firstRow);
-    table.insertBefore(thead, table.firstElementChild);
-
+    if (firstRow) {
+      firstRow.parentNode.removeChild(firstRow);
+      thead.appendChild(firstRow);
+      table.insertBefore(thead, table.firstElementChild);
+    }
+  
     // Remove the first column and put the image in the next.
     const rows = Array.from(table.querySelectorAll('tr'));
     rows.forEach((row) => {
       const iconColumn = row.children[0];
       const fileColumn = row.children[1];
-
+  
+      // Check if the row has the required columns.
+      if (!iconColumn || !fileColumn) {
+        return;
+      }
+  
       // Remove icon column.
       row.removeChild(iconColumn);
-
+  
       const image = iconColumn.firstElementChild;
-
+  
       if (!image) {
         return;
       }
-
+  
       // Wrap icon in a div.img-wrap.
       const div = document.createElement('div');
       div.className = 'img-wrap';
       div.appendChild(image);
-
+  
       // Insert icon before filename.
       fileColumn.insertBefore(div, fileColumn.firstElementChild);
-    });
+    })
+    
   }
-
+  
   // Underscore string's titleize.
   function titleize(str) {
     return decodeURI(str).toLowerCase().replace(/(?:^|\s|-)\S/g, c => c.toUpperCase());
